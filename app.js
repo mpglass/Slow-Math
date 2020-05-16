1e71b15285e10fc330f503ecb06f520b2e6d58d8
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-05-16 00:26:27 -0500

    Async await function works.

--- app.js
+++ app.js
@@ -1,39 +1,68 @@
-slowMath.add(6,2)
-.then((sum) => {
-    console.log(sum);
-    return slowMath.multiply(sum, 2)
-})
-.then((sum) => {
-    console.log(sum);
-    return slowMath.divide(sum, 4)
-})
-.then((sum) => {
-    console.log(sum);
-    return slowMath.subtract(sum, 3)
-})
-.then((sum) => {
-    console.log(sum);
-    return slowMath.add(sum, 98)
-})
-.then((sum) => {
-    console.log(sum);
-    return slowMath.remainder(sum, 2)
-})
-.then((sum) => {
-    console.log(sum);
-    return slowMath.multiply(sum, 50)
-})
-.then((sum) => {
-    console.log(sum);
-    return slowMath.remainder(sum, 40)
-})
-.then((sum) => {
-    console.log(sum);
-    return slowMath.add(sum, 32)
-})
-.then((sum) => {
-    console.log(`The final result is ${sum}.`)
-})
-.catch((error) => {
-    console.log(error);
-});
+// slowMath.add(1,1)
+// .then((sum) => {
+//     console.log(sum);
+//     return slowMath.multiply(sum, 2)
+// })
+// .then((sum) => {
+//     console.log(sum);
+//     return slowMath.divide(sum, 4)
+// })
+// .then((sum) => {
+//     console.log(sum);
+//     return slowMath.subtract(sum, 3)
+// })
+// .then((sum) => {
+//     console.log(sum);
+//     return slowMath.add(sum, 98)
+// })
+// .then((sum) => {
+//     console.log(sum);
+//     return slowMath.remainder(sum, 2)
+// })
+// .then((sum) => {
+//     console.log(sum);
+//     return slowMath.multiply(sum, 50)
+// })
+// .then((sum) => {
+//     console.log(sum);
+//     return slowMath.remainder(sum, 40)
+// })
+// .then((sum) => {
+//     console.log(sum);
+//     return slowMath.add(sum, 32)
+// })
+// .then((sum) => {
+//     console.log(`The final result is ${sum}.`)
+// })
+// .catch((error) => {
+//     console.log(error);
+// });
+
+
+const doMath = async () => {
+    try{
+        let result = await slowMath.add(6,2);
+        console.log(result);
+        result = await slowMath.multiply(result, 2);
+        console.log(result);
+        result = await slowMath.divide(result, 4);
+        console.log(result);
+        result = await slowMath.subtract(result, 3);
+        console.log(result);
+        result = await slowMath.add(result, 98);
+        console.log(result);
+        result = await slowMath.remainder(result, 2);
+        console.log(result);
+        result = await slowMath.multiply(result, 50);
+        console.log(result);
+        result = await slowMath.remainder(result, 40);
+        console.log(result);
+        result = await slowMath.add(result, 32);
+        console.log(`The final result is ${result}.`)
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+
+doMath();
